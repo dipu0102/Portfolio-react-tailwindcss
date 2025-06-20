f5bf31c8945d6f09143a6c9c589b0df4a3cd886c
Dipankar <dipankargarai1998@gmail.com>
2025-06-21 00:30:20 +0530

    setup tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-200 underline">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
