4b08de3f7755ebecac43ce46b65ca799c4184501
dipankar <dipankargarai420@gmail.com>
2025-06-21 12:46:18 +0530

    Navbar section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-200 underline">Vite + React</h1>
+      <div className="bg-[#050414]">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+        <div className="relative pt-20 bg-[size:cover]">
+          <Navbar />
+          {/* <About />
+          <Skills />
+          <Experience />
+          <Contact />
+          <Footer /> */}
+        </div>
+      </div>
     </>
   );
 }
